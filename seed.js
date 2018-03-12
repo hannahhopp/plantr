4dd14164abae0027db288585afa2775e14101cb3
hannahhopp <hannah.hopp@gmail.com>
2018-03-12 14:39:25 -0700

    planted one seed

--- seed.js
+++ seed.js
@@ -1,21 +1,19 @@
 const { db, Gardener, Plot, Vegetable } = require('./models')
 
-Vegetable.create({ 
-    name: 'tomato', 
-    planted_on: new Date(2018, 2, 11),
-    })
-
-
-
 db.sync({force: true})
   .then(() => {
+    return Vegetable.create({
+      name: 'tomato',
+      planted_on: new Date(2018, 2, 11),
+      color: 'green'
+    });
+  }).then (() => {
     console.log('Database synced!')
     db.close()
-  })
-  .catch(err => {
+  }).catch(err => {
     console.log('Disaster! Something went wrong! ')
     console.log(err)
     db.close()
-  })
+  });
 
 
